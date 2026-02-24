e17a84fa0f2119d7c461a56f58967876d64697a3
Raj <142092924+rajsingho3@users.noreply.github.com>
2026-02-24 15:49:24 +0530

    dist files removed

--- backend/dist/index.js
+++
@@ -1,7 +0,0 @@
-import { Server } from "socket.io";
-const io = new Server();
-io.on("connection", (socket) => {
-    console.log("a user connected");
-});
-io.listen(3000);
-//# sourceMappingURL=index.js.map
